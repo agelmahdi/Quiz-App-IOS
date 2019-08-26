9bb2c26c7cd17ab46e49f62d2c195f5db1d46012
Ahmed El-Mhadi <agelmahdi@gmail.com>
2019-08-26 21:37:48 +0200

    Initial Commit

---
+++ Quiz App IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Quiz App IOS
+//
+//  Created by Ahmed El-Mahdi on 8/26/19.
+//  Copyright © 2019 Ahmed El-Mahdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
